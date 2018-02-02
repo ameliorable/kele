ebb8c86d63bc6b92e19c4021fcfa942fb17f1139
Carolyn Walcott <carolynoliveira515@gmail.com>
2018-02-02 03:10:39 -0500

    Kele: Mentor Availability - Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,13 +7,18 @@ class Kele
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    response = self.class.post('/sessions', body: { email: email, password: password })
+    response = self.class.post("/sessions", body: { email: email, password: password })
     raise 'Invalid email or password. Authentication failed.' if response.code == 404
     @auth_token = response['auth_token']
   end
 
   def get_me
-    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 end
